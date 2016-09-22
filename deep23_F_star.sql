2efad6994fed01cecbdc13f4a8a444332428ef03
Cheng Cheng <ustccheng@gmail.com>
2016-09-22 12:06:44 -0700

    Update deep23_F_star.sql

--- deep23_F_star.sql
+++ deep23_F_star.sql
@@ -1,10 +1,10 @@
+--http://skyserver.sdss.org/dr12/en/tools/search/sql.aspx
+
 select 
 p.ra, p.dec, p.dered_u, p.dered_g, p.dered_r, p.dered_i, p.dered_z,
 p.extinction_u,p.extinction_g,p.extinction_r,p.extinction_i,p.extinction_z,
 p.objid,p.psfMag_u, p.psfMag_g, p.psfMag_r, p.psfMag_i, 
-p.psfMag_z
-
---into mydb.deep23_f_stars2
+p.psfMag_z, p.type, p.PROBPSF_R
 
 from PhotoObj as p
 where
@@ -18,3 +18,4 @@ p.psfMag_r - p.psfMag_i >= -0.1 and p.psfMag_r - p.psfMag_i <= 0.3
 
 and p.ra  between 352. and 353
 and p.dec between -0.2 and 0.5
+
